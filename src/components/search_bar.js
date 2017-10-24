5555bcae04d0fb2f70e65067090415f85e1c53f1
Miguel Gasca <miguelgasca@gmail.com>
2017-10-24 19:46:33 +0200

    Inicial

---
+++ src/components/search_bar.js
@@ -0,0 +1,12 @@
+/**
+ * Created by miguel on 24/10/17.
+ */
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+    render() {
+        return <input />;
+    }
+}
+
+export default SearchBar;
---
+++ src/components/video_detail.js
@@ -0,0 +1,3 @@
+/**
+ * Created by miguel on 24/10/17.
+ */
