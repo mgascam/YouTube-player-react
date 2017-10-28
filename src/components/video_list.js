27973cf03b5a31c5cb1c043eb109c9711e45bcea
Miguel Gasca <miguelgasca@gmail.com>
2017-10-28 09:37:35 +0200

    Video List

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,3 +1,15 @@
 /**
  * Created by miguel on 24/10/17.
  */
+import React from 'react';
+
+const VideoList = (props) => {
+
+  return (
+    <ul className="col-md4 list-group">
+      {props.videos.length}
+    </ul>
+  );
+}
+
+export default VideoList;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
 /**
  * Created by miguel on 23/10/17.
  */
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyDlfX23tXA4haM8D-qWV1MLFh343IOwqDg';
 
-const App = () => {
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {videos: []};
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
     return (
-        <div>
-            <SearchBar />
-        </div>
+      <div>
+        <SearchBar />
+        <VideoList videos={ this.state.videos } />
+      </div>
     );
-};
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
