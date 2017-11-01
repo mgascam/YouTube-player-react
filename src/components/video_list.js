cb703fb4627e95cabbddf95d35c5c10a87298a8c
Miguel Gasca <miguelgasca@gmail.com>
2017-11-01 20:13:40 +0100

    Video List

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -2,12 +2,16 @@
  * Created by miguel on 24/10/17.
  */
 import React from 'react';
+import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
+  const videoItems = props.videos.map((video) => {
+    return <VideoListItem key={video.etag} video={video} />
+  });
 
   return (
     <ul className="col-md4 list-group">
-      {props.videos.length}
+      {videoItems}
     </ul>
   );
 }
--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,3 +1,22 @@
 /**
  * Created by miguel on 24/10/17.
  */
+import React from 'react';
+
+const VideoListItem = ({video}) => {
+  const imageUrl = video.snippet.thumbnails.default.url;
+  return (
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img src={imageUrl} className="media-object" alt=""/>
+        </div>
+      </div>
+      <div className="media-body">
+        <div className="media-heading">{video.snippet.title}</div>
+      </div>
+    </li>
+  );
+};
+
+export default VideoListItem;
